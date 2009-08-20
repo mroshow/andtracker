770be0b4d924505f73812703fd16a4ce73778cce
camueller28 <camueller28@650f22e8-7931-11de-b03f-d78e5a28ce11>
2009-08-20 17:55:28 +0000

    initial import

---
+++ LiveTracker/src/de/avanux/android/livetracker/Configuration.java
@@ -0,0 +1,145 @@
+/* Copyright (C) 2009  Axel Müller <axel.mueller@avanux.de> 
+ * 
+ * This file is part of LiveTracker.
+ * 
+ * LiveTracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LiveTracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LiveTracker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.avanux.android.livetracker;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+
+public class Configuration extends PropertiesStringParser implements OnSharedPreferenceChangeListener {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final int SERVER_API_VERSION = 1;
+	
+	private static final String TAG = "LiveTracker:Configuration";
+
+    private static final String SERVER_BASE_URL = "http://miraculix.localnet:8080/LiveTrackerServer";
+    // private static final String SERVER_BASE_URL = "http://livetracker.dyndns.org";
+	
+	private Long timeInterval;
+
+    private Long minTimeInterval;
+	
+	private static String timeIntervalPreferenceKey;
+
+	private Float distance;
+	
+	private static String distancePreferenceKey;
+
+	
+	public Configuration(String propertiesString) throws IOException {
+		super(propertiesString);
+	}
+
+	public static String getServerBaseUrl() {
+	    return SERVER_BASE_URL;
+	}
+	
+    public boolean isMatchingServerApiVersion() {
+		if(Integer.parseInt(getProperties().getProperty(ConfigurationConstants.SERVER_API_VERSION)) == SERVER_API_VERSION) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public String getID() {
+		return getProperties().getProperty(ConfigurationConstants.ID);
+	}
+
+	
+	public long getTimeInterval() {
+		if (timeInterval != null) {
+			return this.timeInterval;
+		} else {
+			return Long.parseLong(getProperties().getProperty(ConfigurationConstants.TIME_INTERVAL));
+		}
+	}
+	public void setTimeInterval(long timeInterval) {
+		this.timeInterval = timeInterval;
+	}
+
+    public static long getDefaultTimeInterval() {
+        return 1;
+    }
+	
+    public long getMinTimeInterval() {
+        if (minTimeInterval != null) {
+            return this.minTimeInterval;
+        } else {
+            return Long.parseLong(getProperties().getProperty(ConfigurationConstants.MIN_TIME_INTERVAL));
+        }
+    }
+    public void setMinTimeInterval(long minTimeInterval) {
+        this.minTimeInterval = minTimeInterval;
+    }
+	
+	public float getDistance() {
+		if (this.distance != null) {
+			return distance;
+		} else {
+			return Float.parseFloat(getProperties().getProperty(ConfigurationConstants.DISTANCE));
+		}
+	}
+	public void setDistance(float distance) {
+		this.distance = distance;
+	}
+
+
+    public static long getDefaultDistance() {
+        return 0;
+    }
+	
+    public float getMinDistance() {
+		return Float.parseFloat(getProperties().getProperty(ConfigurationConstants.MIN_DISTANCE));
+    }
+
+    public String getMessageToUsers() {
+        return getProperties().getProperty(ConfigurationConstants.MESSAGE_TO_USERS);
+    }
+
+	public String getLocationReceiverUrl() {
+        return getProperties().getProperty(ConfigurationConstants.LOCATION_RECEIVER_URL);
+	}
+
+	//
+    // ~ Preferences ----------------------------------------------------------------------------------------------------
+	//
+	
+    public static void setTimeIntervalPreferenceKey(String timeIntervalPreferenceKey) {
+        Configuration.timeIntervalPreferenceKey = timeIntervalPreferenceKey;
+    }
+
+    public static void setDistancePreferenceKey(String distancePreferenceKey) {
+        Configuration.distancePreferenceKey = distancePreferenceKey;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals(this.timeIntervalPreferenceKey)) {
+            setTimeInterval(Long.parseLong(sharedPreferences.getString(this.timeIntervalPreferenceKey, "" + getDefaultTimeInterval())));
+        }
+        else if (key.equals(this.distancePreferenceKey)) {
+            setDistance(Float.parseFloat(sharedPreferences.getString(this.distancePreferenceKey, "" + getDefaultDistance())));
+        }
+    }
+}
